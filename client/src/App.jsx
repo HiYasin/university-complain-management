29224f807da8938ca129da6a9bb96291629b3cfc
HiYasin <hello.mr.faisal@gmail.com>
2025-07-31 18:43:07 +0600

    chore: setup react project with tailwind, shadcn, react-router and configure theme-toggle

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+import { ModeToggle } from "./components/ui/mode-toggle"
+
+function App() {
+
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <Button>Hello</Button>
+      <ModeToggle />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { ThemeProvider } from './components/theme-provider'
+import { RouterProvider } from 'react-router'
+import router from './routes/routes'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>,
+)
