42791c535ae3cc38b2c95d2a5a13b23113fc935b
HiYasin <hello.mr.faisal@gmail.com>
2025-08-08 16:16:17 +0600

    feat: add private route and dummy pages

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <h1 className='text-5xl'>This is Dashboard</h1>
+    </div>
+  )
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center w-full h-screen">
+    <div className="flex justify-center items-center w-full h-screen flex-col">
+      <nav>
+        <ul className="flex space-x-4">
+          <li><Link to="/auth" className="text-blue-500">Auth Page</Link></li>
+          <li><Link to="/" className="text-blue-500">Home</Link></li>
+          <li><Link to="/dashboard" className="text-blue-500">Dashboard</Link></li>
+        </ul>
+      </nav>
       <h1 className='text-5xl'>This is homepage</h1>
     </div>
   )
