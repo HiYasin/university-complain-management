00145baef3176a7c7149ea66ba994b383e05711b
HiYasin <hello.mr.faisal@gmail.com>
2025-08-08 16:48:21 +0600

    chore: create home layout

--- client/src/layouts/HomeLayout.jsx
+++ client/src/layouts/HomeLayout.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Outlet } from 'react-router'
 
 export default function HomeLayout() {
-  return (
-    <div>HomeLayout</div>
-  )
+    return (
+        <div className="flex flex-col min-h-screen">
+            <header>
+                {/* Add the navber or header component here */}
+            </header>
+            <main className="flex-grow">
+                <Outlet />
+            </main>
+            <footer>
+                {/* add the footer here */}
+            </footer>
+        </div>
+    )
 }
